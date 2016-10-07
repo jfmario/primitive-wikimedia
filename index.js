fd4aebcf81d5f4400182ff3464bfeb54bbcedf84
John Francis Marion <john.f.marion@gmail.com>
2016-10-07 15:54:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ for ( var i = 0; i < config.primitive_pic.conversions.length; ++i )
     
     var conversion = config.primitive_pic.conversions [i];
     console.log ( conversion.description );
-    var command = config.primitive_pic.path + ' -i ' + path.resolve ( __dirname, 'node_modules', 'wikimedia-commons', 'data', 'latest.jpg' ) +
+    var command = config.primitive_pic.path + ' -i ' + path.resolve ( __dirname, 'node_modules', 'wikimedia-commons', 'output', 'latest.jpg' ) +
         ' -o ' + path.resolve ( __dirname, 'tmp', conversion.slug + '.jpg' ) + ' -m ' + conversion.mode + ' -n ' + conversion.iterations;
     
     childProcess.execSync ( command );
