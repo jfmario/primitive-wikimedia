ab14fe402e89c094f7bb6905d669493a690fee36
John Francis Marion <john.f.marion@gmail.com>
2016-10-07 15:09:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 /**
  * Runs the daily process.
  */
- 
+
+var childProcess = require ( 'child-process' );
+var path = require ( 'path' );
+var yaml = require ( 'yaml' );
 var wikimediaCommons = require ( 'wikimedia-commons' );
+
+var config = yaml.load ( path.resolve ( __dirname, 'config.yml' ) )
+
+console.log ( "Getting daily image from Wikimedia Commons." );
+
 wikimediaCommons.dailypic ();
+
+console.log ( "Using PrimitivePic to generate abstract variants." )
+for ( var i = 0; i < config.primitive_pic.conversions.length; ++i )
+{
+    
+    var conversion = config.primitive_pic.conversions [i];
+    console.log ( conversion.description );
+    var command = config.primitive_pic.path + ' -i ' + path.resolve ( __dirname, 'node_modules', 'wikimedia-commons', 'data', 'latest.jpg' ) +
+        ' -o ' + path.resolve ( __dirname, 'tmp', conversion.slug + '.jpg' ) + ' -m ' + conversion.mode + ' -n ' + conversion.iterations;
+    
+    chci
